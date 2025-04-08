dd6f95af810b21c483fa0fc219e52e9da3ca0538
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-04-08 22:04:02 +0200

    add onChange event on inputs

--- client/src/components/AddForm.tsx
+++ client/src/components/AddForm.tsx
@@ -1,18 +1,29 @@
 import { GiCancel } from "react-icons/gi";
 import Input from "./Input";
+import { useState } from "react";
 
 interface FormProps {
     onSelect: ()=>void
 }
 
 const AddForm: React.FC<FormProps> = ({onSelect})=>{
+    const [title, setTitle]= useState<string>('')
+    const [desc, setDesc]= useState<string>('')
+
+const handleSave= (event: any)=> {
+    event.preventDefault();
+    // console.log('titolo', title + ' descrizione', desc);
+    setTitle('');
+    setDesc('');
+    
+}
     return (
-        <form action="" className="shadow rounded-xl bg-[#4C5B61] min-w-82 p-5 flex flex-col gap-5 relative text-white">
-            <Input label="Title"/>
-            <Input label="Description" isTextArea={true}/>
+        <form action="" className="shadow rounded-xl bg-[#4C5B61] min-w-82 p-10 flex flex-col gap-5 relative text-white">
+            <Input label="Title" value={title} handleChange={(e: any)=>setTitle(e.target.value)}/>
+            <Input label="Description" isTextArea={true} value={desc} handleChange={(e: any)=>setDesc(e.target.value)}/>
             <GiCancel onClick={onSelect} className="absolute right-2 top-2 cursor-pointer"/>
             <div className="flex justify-center">
-                <button type="submit" className="hover:border-white border-b-1 border-transparent cursor-pointer">Save</button>
+                <button onClick={(e)=>handleSave(e)} type="submit" className="hover:border-white border-b-1 border-transparent cursor-pointer">Save</button>
             </div>
         </form>
     )
--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,16 +1,18 @@
 interface InputProps {
     label:string,
     isTextArea?: boolean
+    value: string
+    handleChange: (e: any)=> void,
 }
 
-const Input: React.FC<InputProps> = ({isTextArea, label, ...props})=>{
+const Input: React.FC<InputProps> = ({isTextArea, value, handleChange, label, ...props})=>{
     return (
         <div className="flex flex-col gap-1">
             <label htmlFor="">{label}</label>
             {isTextArea ?
-                <textarea {...props} className="border rounded px-1 py-1 focus:outline-none shadow focus:shadow-[#7ab2b8]"/>
+                <textarea onChange={handleChange} value={value} {...props} className="border rounded px-1 py-1 focus:outline-none shadow focus:shadow-[#7ab2b8]"/>
                 :
-                <input {...props} className="border rounded px-1 py-1 focus:outline-none shadow focus:shadow-[#7ab2b8]"/>
+                <input onChange={handleChange} value={value} {...props} className="border rounded px-1 py-1 focus:outline-none shadow focus:shadow-[#7ab2b8]"/>
             }
         </div>
     )
